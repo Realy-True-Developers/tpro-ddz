72f19e694648a27a3a6384d44393132290d82514
4ylovek <147433955+4ylovek@users.noreply.github.com>
2023-11-12 18:17:32 +0300

    Изменил логику движения дудла в стороны, изменил логику прыжков, добавил рандомную генерацию 5 платформ на экране
    не удалось реализовать отпрыгивание дудла от платформы

--- executable/main.cpp
+++ executable/main.cpp
@@ -1,58 +1,102 @@
 #include <SFML/Graphics.hpp>
 #include <thread>
+#include <random>
+
+#include "../game_menu/game_menu.hpp"
 
 int main(){
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
     // Объект, который, собственно, является главным окном приложения
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Game", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(800, 1000), "Game", sf::Style::Default, settings);
 
-    sf::RectangleShape Doodle(sf::Vector2f(120.f, 300.f));
+    int WinSizeX = window.getSize().x;
+    int WinSizeY = window.getSize().y;
+
+    sf::RectangleShape plat1(sf::Vector2f(150.f, 30.f));
+    sf::RectangleShape plat2(sf::Vector2f(150.f, 30.f));
+    sf::RectangleShape plat3(sf::Vector2f(150.f, 30.f));
+    sf::RectangleShape plat4(sf::Vector2f(150.f, 30.f));
+    sf::RectangleShape plat5(sf::Vector2f(150.f, 30.f));
+
+    sf::RectangleShape platforms[5]{plat1,plat2,plat3,plat4,plat5};
+    for (size_t i = 0; i < 5; ++i) {
+        platforms[i].setOrigin(75,15);
+        platforms[i].setFillColor(sf::Color::Cyan);
+        float x = rand()%(WinSizeX-75);
+        float y = rand()%(WinSizeY-15);
+        platforms[i].setPosition(x,y);
+    }
+    
+    
+    
+    sf::RectangleShape Doodle(sf::Vector2f(120.f, 150.f));
     Doodle.setFillColor(sf::Color::Green);
     Doodle.setOrigin(sf::Vector2f(Doodle.getSize().x / 2, Doodle.getSize().y / 2));
-    Doodle.setPosition(sf::Vector2f(static_cast<float>(window.getSize().x / 2),
-                                   static_cast<float>(window.getSize().y - 10.0f)));
+    float dY=0;
 
-    sf::Clock* clock = new sf::Clock;
-    sf::Clock* upClock= new sf::Clock;
-    float dT;
-    float upTime = 0;
-    float speed = 3000;
-    float YSpeed = 1;
-    while (window.isOpen())
-    {
-        dT = clock->restart().asSeconds();
-        if (upTime>=10){
-            upTime=upClock->restart().asSeconds();
-            upClock->restart();
-        }
-        else{
-            upTime=upClock->getElapsedTime().asSeconds();
-        }
+    float DoodleX=WinSizeX / 2;
+    float DoodleY=WinSizeY - 100.0f;
+    Doodle.setPosition(sf::Vector2f(static_cast<float>(DoodleX),
+                                   static_cast<float>(DoodleY)));
+
+    bool isUp=true; //флаг, показывает поднимается дудл или нет
+    
+    while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-                Doodle.move(-1.0f* speed*dT, 0.0f);
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-                Doodle.move(1.0f* speed*dT, 0.0f);
-            if (upTime<=5||upTime>10){
-                Doodle.move(0.0f,-YSpeed*upTime);
-            }
-            else{
-                Doodle.move(0.0f,YSpeed*upTime);
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+                window.close();
             }
         }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+                DoodleX-=0.07f;
+                }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)){
+                DoodleX+=0.07f;
+            }
+            
+        if(isUp)
+            DoodleY+=0.1f;
+        else
+            DoodleY-=0.1f;
+
+        if (DoodleX<=-Doodle.getSize().x/2)
+            DoodleX=WinSizeX+Doodle.getSize().x/2-1;
+        if(DoodleX>=WinSizeX+Doodle.getSize().x/2)
+            DoodleX=-Doodle.getSize().x/2+1;
+
+        if(DoodleY>=WinSizeY-Doodle.getSize().y/2)
+                isUp=false;
+            if(DoodleY<=Doodle.getSize().y/2)
+                isUp=true;
+            
+        if (!isUp)
+        {
+            for (size_t i = 0; i < 5; ++i)
+            {
+                if (DoodleY+75.f==platforms[i].getPosition().y-15&&(platforms[i].getPosition().x-75<=DoodleX&&DoodleX<=platforms[i].getPosition().x+75))
+                {
+                    isUp=true;
+                }
+            }
+            
+        }
+        
+        Doodle.setPosition(DoodleX, DoodleY);
 
         window.clear(sf::Color(255, 255, 255, 0));
 
-        window.draw(Doodle);
+        for (size_t i = 0; i < 5; ++i){
+            window.draw(platforms[i]);
+        }
 
+        window.draw(Doodle);
         window.display();
     }
-
     return 0;
 }
