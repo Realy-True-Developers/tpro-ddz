4c155990181dafce4546dcaf30b31b53c675f917
4ylovek <4ylovek@gitwork.ru>
2023-11-13 22:58:25 +0300

    Реализовал отскок платформы, начал работу над меню паузы(затемнение экрана, остановка обработки игры)

--- executable/main.cpp
+++ executable/main.cpp
@@ -7,12 +7,18 @@
 int main(){
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
+    
+    bool gamepause=true; //Флаг паузы
 
     // Объект, который, собственно, является главным окном приложения
-    sf::RenderWindow window(sf::VideoMode(800, 1000), "Game", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(900, 1000), "Game", sf::Style::Default, settings);
 
     int WinSizeX = window.getSize().x;
-    int WinSizeY = window.getSize().y;
+    int WinSizeY = window.getSize().y; //Переменные размера окна, чтобы не вызвать постоянно функции
+
+    sf::RectangleShape pause(sf::Vector2f(WinSizeX,WinSizeY));
+    pause.setFillColor(sf::Color(122,122,122,200));
+    pause.setPosition(sf::Vector2f(0,0)); //Прямоугольник, затемняющий экран при выходе в меню паузы
 
     sf::RectangleShape plat1(sf::Vector2f(150.f, 30.f));
     sf::RectangleShape plat2(sf::Vector2f(150.f, 30.f));
@@ -20,21 +26,21 @@ int main(){
     sf::RectangleShape plat4(sf::Vector2f(150.f, 30.f));
     sf::RectangleShape plat5(sf::Vector2f(150.f, 30.f));
 
-    sf::RectangleShape platforms[5]{plat1,plat2,plat3,plat4,plat5};
+    sf::RectangleShape platforms[5]{plat1,plat2,plat3,plat4,plat5}; //Массив платформ для простоты работы с ними
+
+    float x, y;
     for (size_t i = 0; i < 5; ++i) {
-        platforms[i].setOrigin(75,15);
+        platforms[i].setOrigin(platforms[i].getSize().x / 2, platforms[i].getSize().y / 2);
         platforms[i].setFillColor(sf::Color::Cyan);
-        float x = rand()%(WinSizeX-75);
-        float y = rand()%(WinSizeY-15);
+        x = rand()%(WinSizeX-75);
+        y = rand()%(WinSizeY-15);
         platforms[i].setPosition(x,y);
     }
-    
-    
+
     
     sf::RectangleShape Doodle(sf::Vector2f(120.f, 150.f));
     Doodle.setFillColor(sf::Color::Green);
-    Doodle.setOrigin(sf::Vector2f(Doodle.getSize().x / 2, Doodle.getSize().y / 2));
-    float dY=0;
+    Doodle.setOrigin(sf::Vector2f(Doodle.getSize().x / 2, Doodle.getSize().y / 2)); //Создаём попрыгунчика, ставим центр его координат в центр прямоугольника
 
     float DoodleX=WinSizeX / 2;
     float DoodleY=WinSizeY - 100.0f;
@@ -48,11 +54,29 @@ int main(){
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-                window.close();
+
+            if (event.type == sf::Event::KeyReleased && event.key.code==sf::Keyboard::Escape)
+            {
+                sf::Event pauseEvent;
+                gamepause=true;
+                for (size_t i = 0; i < 5; ++i){
+                        window.draw(platforms[i]);
+                    }
+                    window.draw(Doodle);
+                    window.draw(pause);
+                    window.display();
+                while (gamepause)
+                {
+                    while (window.pollEvent(pauseEvent)){
+                        if (pauseEvent.type == sf::Event::Closed)
+                            window.close();               
+                        if(pauseEvent.type == sf::Event::KeyReleased && pauseEvent.key.code==sf::Keyboard::Escape){
+                            gamepause=false;
+                        }
+                    }
+                }
             }
         }
-
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
                 DoodleX-=0.07f;
                 }
@@ -61,35 +85,37 @@ int main(){
             }
             
         if(isUp)
-            DoodleY+=0.1f;
-        else
             DoodleY-=0.1f;
+        else
+            DoodleY+=0.1f;
 
         if (DoodleX<=-Doodle.getSize().x/2)
             DoodleX=WinSizeX+Doodle.getSize().x/2-1;
         if(DoodleX>=WinSizeX+Doodle.getSize().x/2)
             DoodleX=-Doodle.getSize().x/2+1;
 
-        if(DoodleY>=WinSizeY-Doodle.getSize().y/2)
+        if(DoodleY<=Doodle.getSize().y/2)
                 isUp=false;
-            if(DoodleY<=Doodle.getSize().y/2)
-                isUp=true;
-            
-        if (!isUp)
-        {
-            for (size_t i = 0; i < 5; ++i)
-            {
-                if (DoodleY+75.f==platforms[i].getPosition().y-15&&(platforms[i].getPosition().x-75<=DoodleX&&DoodleX<=platforms[i].getPosition().x+75))
-                {
-                    isUp=true;
-                }
-            }
-            
-        }
         
+        if(DoodleY>=WinSizeY+Doodle.getSize().y/2){
+            goto gameover;
+        }
+     
         Doodle.setPosition(DoodleX, DoodleY);
 
-        window.clear(sf::Color(255, 255, 255, 0));
+        if (!isUp){ // дудл падает
+            for (size_t i = 0; i < 5; ++i){
+                if ((platforms[i].getPosition().y-15<=DoodleY+75)&& //Нижний край дудла ниже верхнего края платформы
+                (DoodleY+75<=platforms[i].getPosition().y+15)&& //Нижний край дудла выше нижнего края платформы
+                (platforms[i].getPosition().x-75<DoodleX+60)&& 
+                (DoodleX-60<platforms[i].getPosition().x+75)){ //Дудл попадает на платформу хотя бы краем своего тела
+                    isUp=true;
+                    break;
+                }
+            }
+        }
+
+        window.clear(sf::Color(255, 255, 255));
 
         for (size_t i = 0; i < 5; ++i){
             window.draw(platforms[i]);
@@ -98,5 +124,13 @@ int main(){
         window.draw(Doodle);
         window.display();
     }
+
+
+    gameover:{
+        window.clear(sf::Color(255, 255, 255));
+        return 0;
+    }
+
+    
     return 0;
 }
