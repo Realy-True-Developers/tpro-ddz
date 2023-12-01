482fdb13a23853b4e4d1723d1ea8d59469c4242e
Lucifer <147835077+LuciferMatthew@users.noreply.github.com>
2023-12-01 17:32:02 +0300

    сделал текстуры всем видам платформ

--- platforms/platforms.hpp
+++ platforms/platforms.hpp
@@ -1,8 +1,9 @@
+#pragma once
 #include <random>
 #include <ctime>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <algorithm>
+
 using namespace std;
 
 enum PlatType{
@@ -34,4 +35,8 @@ public:
     sf::Color _color;
     bool IsRight;
     bool IsJumped;
+    bool GreenP;
+    bool BrownP;
+    bool BlueP;
+    bool GreyP;
 };
